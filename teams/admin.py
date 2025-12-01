22fb034234b0c0a4cc86b10992023c5acf74d994
Emma Spear <emmspear@gmail.com>
2025-12-01 14:19:05 +0800

    registered teams models in admin

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Team, TeamMembership
 
-# Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_by', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['name', 'description']
+
+@admin.register(TeamMembership)
+class TeamMembershipAdmin(admin.ModelAdmin):
+    list_display = ['id', 'team', 'user', 'joined_at']
+    list_filter = ['team', 'joined_at']
+    search_fields = ['team__name', 'user__username']
